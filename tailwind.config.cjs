330603cd28776a5eb7dec72813abeee90e598b91
William Kaiser <wkaisertexas@gmail.com>
2023-07-13 23:56:52 -0500

    changed text color to medium color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+      textColor: {
+        'default': '#242424',
+      },
       fontFamily: {
         sans: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
       },
