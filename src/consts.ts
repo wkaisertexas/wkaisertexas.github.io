cb2ef2ebe1fea0195ab095533df35c9c86d530e9
William Kaiser <wkaisertexas@gmail.com>
2023-07-14 23:45:09 -0500

    fixed pesky alignment issues

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'William Kaiser\'s Blog';
+export const SITE_TITLE = 'William Kaiser\'s Portfolio';
 
 export const SITE_DESCRIPTION = 'A place for my projects and thoughts.';
 export const PROJECTS_DESCRIPTION = "A collection of my projects";
