b9016dfefecfa76741aea78ccca4fdfc979ae45e
William Kaiser <wkaisertexas@gmail.com>
2023-07-14 22:21:16 -0500

    Cleaned up home

--- src/consts.ts
+++ src/consts.ts
@@ -10,8 +10,9 @@ export const BLOGS_DESCRIPTION = "A collection of my blogs";
 export const GOOGLE_ANALYTICS_ID = 'G-XKDGVM797T';
 
 export const EMAIL = "wkaisertexas@gmail.com";
-export const LINKEDIN = "https://linkedin.com/in/williamdkaiser";;
-export const GITHUB = "https://github.com/wkaisertexas"
+export const LINKEDIN = "https://linkedin.com/in/williamdkaiser";
+export const INSTAGRAM = "https://instagram.com/wdkaiser";
+export const GITHUB = "https://github.com/wkaisertexas";
 
 export const NUM_LANGUAGES = 6;
 export const NUM_BLOGS = 4;
